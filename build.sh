3d900bc5962872d3da3b7411d4a484bf1ac56a3f
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2022-09-01 02:26:01 +0530

    modified docker

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-docker build -t anilyerramasu/nnunet .
+docker build -t anilyerramasu/nnunet:beta .
 After building the image using docker build command,  the container using the following:
 
 Run Command
-docker run anilyerramasu/nnunet-spleen --gpus all --rm -v /home/ubuntu/DeepMD-nnunet-Docker/input:/home/nnUNet/input -v /home/ubuntu/DeepMD-nnunet-Docker/output:/home/nnUNet/output
+docker run anilyerramasu/nnunet-spleen:beta --gpus all --rm -v /home/ubuntu/DeepMD-nnunet-Docker/input:/home/nnUNet/input -v /home/ubuntu/DeepMD-nnunet-Docker/output:/home/nnUNet/output
