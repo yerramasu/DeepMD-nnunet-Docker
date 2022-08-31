1b64775d8bc13b97c4caf736e5e9af605ba4a015
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2022-09-01 01:16:59 +0530

    modified docker

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@ docker build -t anilyerramasu/nnunet .
 After building the image using docker build command,  the container using the following:
 
 Run Command
-docker run --gpus "device=1" --ipc=host --name=deepmd_nnUNet -it --rm -v /vault/aberg_vault:/workspace/nnUNet deepmd-nnunet:v3.0-12.31.2020-1
 
-docker run --gpus "device=1" --ipc=host --name=deepmd_nnUNet -it --rm -v /home/ubuntu/DeepMD-nnunet-Docker:/workspace/nnUNet anilyerramasu/nnunet
+docker run --rm --gpus all  --name=anilyerramasu/nnunet-spleen -v /home/ubuntu/DeepMD-nnunet-Docker/input:/home/nnUNet/input -v /home/ubuntu/DeepMD-nnunet-Docker/output:/home/nnUNet/output
