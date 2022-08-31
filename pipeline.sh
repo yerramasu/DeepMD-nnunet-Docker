e6dbec3d7ead85f17952140c24d427714a6fb669
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2022-09-01 01:36:39 +0530

    modified docker

--- pipeline.sh
+++ pipeline.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nnUNet_predict -i /home/nnUNet/input -o /home/nnUNet/output -t 9 -m 3d_fullres
+nnUNet_predict -i /home/nnUNet/input -o /home/nnUNet/output --task_name "Task055_SegTHOR" -m 3d_fullres
