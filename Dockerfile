adf4e575ad7305c726f76772b3c94766003d8d32
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2022-09-01 10:56:43 +0530

    modified docker

--- Dockerfile
+++ Dockerfile
@@ -18,6 +18,7 @@ RUN wget  -O /home/models/Task055_SegTHOR.zip https://www.dropbox.com/s/m7es2ojn
 COPY pipeline.sh /home
 # Installing nnU-Net
 RUN cd /home && \
+  RUN mkdir /{in,out} && \
   git clone https://github.com/MIC-DKFZ/nnUNet.git  && \
   mkdir /home/nnUNet/input && \
 #   mkdir /home/models && \
--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ docker build -t anilyerramasu/nnunet:beta .
 After building the image using docker build command,  the container using the following:
 
 Run Command
-docker run anilyerramasu/nnunet-spleen:beta --gpus all --rm -v /home/ubuntu/DeepMD-nnunet-Docker/input:/home/nnUNet/input -v /home/ubuntu/DeepMD-nnunet-Docker/output:/home/nnUNet/output
+docker run anilyerramasu/nnunet-spleen:beta --gpus all --rm -v /home/ubuntu/DeepMD-nnunet-Docker/input:/home/in -v /home/ubuntu/DeepMD-nnunet-Docker/output:/home/out
