479ac45fed6bc02d050d71158e64c2d33f86eccd
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2022-09-07 23:49:46 +0530

    Added pancreas

--- build.sh
+++ build.sh
@@ -5,5 +5,13 @@ Run Command
 docker run --gpus all   -d --ipc=host --rm -p 5000:5000 -v $(pwd)/input:/home/input -v $(pwd)/output:/home/output  anilyerramasu/segabdominalorgan_3d_gpu 
 
 
+docker build --no-cache -t anilyerramasu/segpancreas_3d_gpu .
+
+
+docker run --gpus all --ipc=host --rm -p 5000:5000 -v $(pwd)/input:/home/input -v $(pwd)/output:/home/output anilyerramasu/seghepaticvessel_3d_gpu
+
+
+
+
 
 
