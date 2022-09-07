d5b3f1afdad465f1b38b2fa8bb2dd3443ce899c1
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2022-09-07 11:07:38 +0530

    added thoractic

--- Thoractic/predict.sh
+++ Thoractic/predict.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -f /home/output/*
 
-nnUNet_predict -i /home/input -o /home/output --task_name "Task003_Liver" -m  3d_fullres
+nnUNet_predict -i /home/input -o /home/output --task_name "Task055_SegTHOR" -m  3d_fullres
