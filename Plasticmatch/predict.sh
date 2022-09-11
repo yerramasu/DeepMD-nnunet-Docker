45c1b24ef78e8ef12ad0c165cc0ec2893e68f517
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2022-09-11 23:43:14 +0530

    Added plasticmatch

--- Plasticmatch/predict.sh
+++ Plasticmatch/predict.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-rm -f /home/output/*
-echo $(plastimatch --version)
-segimage2itkimage --help
-echo $(svn --version | head -n 2)
+# rm -f /home/output/*
+echo $(plastimatch --version) &&
+segimage2itkimage --help &&
+echo $(svn --version | head -n 2) &&
+cd /home/output &&
 # nnUNet_predict -i /home/input -o /home/output --task_name "Task017_AbdominalOrganSegmentation" -m  3d_fullres
+plasticmatch --input /home/input output-img outfile_0001.nii.gz 
