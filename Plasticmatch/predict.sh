67501354cc6641d8f456d685f10ef8e26d79fb4d
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2022-09-19 00:58:33 +0530

    updated plastimatch

--- Plasticmatch/predict.sh
+++ Plasticmatch/predict.sh
@@ -5,4 +5,4 @@ echo $(plastimatch --version) &&
 cd /home/output &&
 
 # nnUNet_predict -i /home/input -o /home/output --task_name "Task017_AbdominalOrganSegmentation" -m  3d_fullres
-plastimatch convert --input /home/input --output-img outfile_0000.nii.gz &&
+plastimatch convert --input /home/input --output-img outfile_0000.nii.gz 
