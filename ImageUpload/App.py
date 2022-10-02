764d8361ca4db7a615b4090a67b9e576e7ce431a
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2022-10-02 18:55:08 +0530

    modified imageuploader

--- ImageUpload/App.py
+++ ImageUpload/App.py
@@ -51,7 +51,16 @@ def home():
 
 @app.route('/dicom/upload', methods=['POST'])
 def upload():
-    
+    folder = config['UPLOAD_FOLDER'];
+    for filename in os.listdir(folder):
+      file_path = os.path.join(folder, filename)
+      try:
+          if os.path.isfile(file_path) or os.path.islink(file_path):
+              os.unlink(file_path)
+          elif os.path.isdir(file_path):
+              shutil.rmtree(file_path)
+      except Exception as e:
+          print('Failed to delete %s. Reason: %s' % (file_path, e)
     if request.method == 'POST':
 
 
