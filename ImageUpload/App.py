4774a50529e5e2b4908603d7747e78056db50bc8
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2022-10-03 19:35:45 +0530

    fixed imageupload

--- ImageUpload/App.py
+++ ImageUpload/App.py
@@ -53,14 +53,25 @@ def home():
 def upload():
     folder = config['UPLOAD_FOLDER'];
     for filename in os.listdir(folder):
+      
       file_path = os.path.join(folder, filename)
       try:
-          if os.path.isfile(file_path) or os.path.islink(file_path):
-              os.unlink(file_path)
-          elif os.path.isdir(file_path):
-              shutil.rmtree(file_path)
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+          os.unlink(file_path)
+        elif os.path.isdir(file_path):
+          shutil.rmtree(file_path)
+
       except Exception as e:
-          print('Failed to delete %s. Reason: %s' % (file_path, e)
+        print('Failed to delete file')
+
+
+      #   if os.path.isfile(file_path) or os.path.islink(file_path):
+      #       os.unlink(file_path)
+      #   elif os.path.isdir(file_path):
+      #       shutil.rmtree(file_path)
+      # except Exception as e:
+      #     print('Failed to delete %s. Reason: %s' % (file_path, e)
+    
     if request.method == 'POST':
 
 
