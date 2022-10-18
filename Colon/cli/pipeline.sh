4e633716a687dbb5483badc2642ac7da5d0a7eeb
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2022-10-18 17:27:39 +0530

    Colon  mvp1

---
+++ Colon/cli/pipeline.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ls /home/nnUNet/input
+cd /home
+# nohup python App.py &
+rm /home/output/*
+# 2d --disable_tta 
+# nnUNet_predict -i /home/input -o /home/output --task_name "Task003_Liver" -m  3d_fullres
+nnUNet_predict -i /home/input -o /home/output --task_name "Task010_Colon" --model 2d --disable_tta 
+
+
