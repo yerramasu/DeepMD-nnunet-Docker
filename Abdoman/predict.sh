6400980aac3bf158bde4b0913bb735a992e6b6f9
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2022-11-30 00:50:24 +0530

    added deepmd-ml ns

--- Abdoman/predict.sh
+++ Abdoman/predict.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # rm -f /home/output/*
-# rm /home/output/*
+rm /home/output/*
 # nnUNet_predict -i /home/input -o /home/output --task_name "Task017_AbdominalOrganSegmentation" --model 2d --disable_tta
 nnUNet_predict -i $inputDir -o $outDir --task_name "Task017_AbdominalOrganSegmentation" --model 2d --disable_tta
 # cp -r $inputDir/* /home/input &&
