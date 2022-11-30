9845c370b68c9cc2e3278efe8488fcc53d06d17a
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2022-11-30 11:58:37 +0530

    upfate plasticmatch

--- Plasticmatch/predict.sh
+++ Plasticmatch/predict.sh
@@ -4,10 +4,10 @@
 # rm  /home/output/*
 # pip3 install dicom2nifti
 cd output 
-ls -al /input | wc
+ls -al ./input | wc
 # cd /home
 # python3 dcm2nifty.py
 # cd output
 # mv *.nii.gz outfile_0000.nii.gz
 # nnUNet_predict -i /home/input -o /home/output --task_name "Task017_AbdominalOrganSegmentation" -m  3d_fullres
-plastimatch convert --input input --output-img outfile_0000.nii.gz 
+plastimatch convert --input ./input --output-img ./output/outfile_0000.nii.gz 
