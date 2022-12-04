40f968c2b529f7d712527944c07cfeba0cc3bf00
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2022-12-05 00:47:17 +0530

    added ICH

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ After building the image using docker build command,  the container using the fo
 docker build --no-cache -t anilyerramasu/plasticmatch .
 docker run   --rm -p 9000:5000 -v $(pwd)/input:/home/input -v $(pwd)/output:/home/output anilyerramasu/plasticmatch 
 
+docker build --no-cache -t anilyerramasu/segcolon_3d_gpu .
 
 docker build --no-cache -t anilyerramasu/segliver_3d_gpu .
 docker run   --rm -p 9000:5000 -v $(pwd)/input:/home/input -v $(pwd)/output:/home/output anilyerramasu/segliver_3d_gpu
@@ -15,7 +16,7 @@ docker run --gpus all    --ipc=host --rm -p 5000:5000 -v $(pwd)/input:/home/inpu
 
 docker build --no-cache -t anilyerramasu/segpancreas_3d_gpu .
 
-
+docker build --no-cache -t  anilyerramasu/seghepaticvessel_3d_gpu .
 docker run --gpus all --ipc=host --rm -p 5000:5000 -v $(pwd)/input:/home/input -v $(pwd)/output:/home/output anilyerramasu/seghepaticvessel_3d_gpu
 
 
